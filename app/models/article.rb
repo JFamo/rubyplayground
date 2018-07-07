dc64068bbf46295906568db15b4f34917bbb2780
J. Famous <joshua.famous@gmail.com>
2018-07-07 15:48:21 -0400

    Basic blog - comments, articles, users

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+	belongs_to :user
+	has_many :comments
+	validates :title, presence: true, length: { maximum: 50 }
+	validates :post, presence: true, length: { maximum: 500 }
+	validates :user, presence: true
 end
